bda89c314e5d3c1ce2786eb2f222ecb330936a24
Rumindu De Silva <rumindu.desilva@itelasoft.com>
2024-03-19 09:11:17 +0530

    Revert "Merge branch 'feat-product-detailed-view' into 'main'"
    This reverts merge request !2

--- src/domain/i.product.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "./entity/product";
-
-export interface IProductRepository {
-  getAllProducts(): Promise<Product[] | null>;
-
-  getProductById(id: number): Promise<Product | null>;
-}
