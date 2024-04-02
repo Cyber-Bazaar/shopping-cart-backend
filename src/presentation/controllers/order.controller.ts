1315adb3776c7c20572cd37d6110715b5285a668
Rumindu De Silva <rumindu.desilva@itelasoft.com>
2024-04-02 17:54:45 +0530

    do some changes on response messages

--- src/presentation/controllers/order.controller.ts
+++ src/presentation/controllers/order.controller.ts
@@ -2,8 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import { OrderService } from "../../application/order.service";
 import { Injectable, Inject } from "../../presentation/utils/dIContainer";
 import jwt from "jsonwebtoken";
-
-
 interface ExpressRequest extends Request {
   tokenPayload?: jwt.JwtPayload;
 }
@@ -19,7 +17,7 @@ export class OrderController {
         const orderHistory = await this.orderService.getOrderHistory(req.tokenPayload.sub);
         res.status(200).json({ message: "success", data: orderHistory });
       } catch (error) {
-        console.error("Error while fetching products:", error);
+        console.error("Error while fetching order History:", error);
         return res.status(500).json({ message: "Internal server error" });
       }
     }
@@ -31,10 +29,10 @@ export class OrderController {
   async createOrder(req: ExpressRequest, res: Response) {
     if (req.tokenPayload && req.tokenPayload.sub) {
       try {
-        const order = await this.orderService.createOrder(req.body, req.tokenPayload.sub);
-        res.status(200).json({ message: "success", data: order });
+        await this.orderService.createOrder(req.body, req.tokenPayload.sub);
+        res.status(200).json({ message: "successfully inserted" });
       } catch (error) {
-        console.error("Error while fetching products:", error);
+        console.error("Error while checkout:", error);
         return res.status(500).json({ message: "Internal server error" });
       }
     }
